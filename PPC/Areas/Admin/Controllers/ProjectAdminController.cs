a00221c39997bb663f74af98663554f28da037b5
hyoriboa <hongocluan1993@gmail.com>
2017-11-26 14:09:01 +0700

    upload multiple Images

--- PPC/Areas/Admin/Controllers/ProjectAdminController.cs
+++ PPC/Areas/Admin/Controllers/ProjectAdminController.cs
@@ -17,52 +17,60 @@ namespace PPC.Areas.Admin.Controllers
             var project = db.PROPERTY.OrderByDescending(x => x.ID).ToList();
             return View(project);
         }
+
         [HttpGet]
         public ActionResult Edit(int id)
         {
             var project = db.PROPERTY.FirstOrDefault(x => x.ID == id);
-            ViewBag.property_type = db.PROPERTY_TYPE.OrderByDescending(x=>x.ID==id).ToList();
-            ViewBag.district = db.DISTRICT.OrderByDescending(x => x.ID == id).ToList();
-            ViewBag.ward = db.WARD.OrderByDescending(x => x.ID == id).ToList();
-            ViewBag.street = db.STREET.OrderByDescending(x => x.ID == id).ToList();
-            ViewBag.status = db.PROJECT_STATUS.OrderByDescending(x => x.ID == id).ToList();
-            ViewBag.sale = db.USER.OrderByDescending(x => x.ID == id).ToList();
+            ViewBag.property_type = db.PROPERTY_TYPE.OrderByDescending(x => x.ID).ToList();
+            ViewBag.district = db.DISTRICT.OrderByDescending(x => x.ID).Where(y => y.ID >= 31 && y.ID <= 54).ToList();
+            ViewBag.ward = db.WARD.OrderByDescending(x => x.ID).Where(y => y.District_ID >= 31 && y.District_ID <= 54).ToList();
+            ViewBag.street = db.STREET.OrderByDescending(x => x.ID).Where(y => y.District_ID >= 31 && y.District_ID <= 54).ToList();
+            ViewBag.status = db.PROJECT_STATUS.OrderByDescending(x => x.ID).ToList();
+            ViewBag.sale = db.USER.OrderByDescending(x => x.ID).ToList();
             return View(project);
         }
+
         [HttpPost]
         public ActionResult Edit(int id, PROPERTY p)
         {
-            //ViewBag.property_type = db.PROPERTY_TYPE.OrderByDescending(x => x.ID).ToList();
-            //ViewBag.district = db.DISTRICT.OrderByDescending(x => x.ID).Where(y => y.ID >= 31 && y.ID <= 54).ToList();
-            //ViewBag.ward = db.WARD.OrderByDescending(x => x.ID).Where(y => y.District_ID >= 31 && y.District_ID <= 54).ToList();
-            //ViewBag.street = db.STREET.OrderByDescending(x => x.ID).Where(y => y.District_ID >= 31 && y.District_ID <= 54).ToList();
-            //ViewBag.status = db.PROJECT_STATUS.OrderByDescending(x => x.ID).ToList();
 
-            var product = db.PROPERTY.FirstOrDefault(x => x.ID == id);
-
-           // string imgs = Images(p);
-            string filename;
-            string extension;
+            PROPERTY product;
             string s;
-            string str = product.Avatar;
+            string b;
+            AvatarU(p, out product, out s);
+            ImagesU(p, out product, out b);
+
+            product.PROPERTY_TYPE = p.PROPERTY_TYPE;
             product.PropertyName = p.PropertyName;
-            product.Price = p.Price;
-       
-            product.UnitPrice = p.UnitPrice;
-            product.BathRoom = p.BathRoom;
-            product.BedRoom = p.BedRoom;
-            product.PackingPlace = p.PackingPlace;
-            //product.USER = p.USER;
-            product.Sale_ID = p.Sale_ID;
+            product.Avatar = s;
+            product.Images = b;
+            product.PropertyType_ID = p.PropertyType_ID;
+            product.Content = p.Content;
             product.Street_ID = p.Street_ID;
             product.Ward_ID = p.Ward_ID;
             product.District_ID = p.District_ID;
-            product.Status_ID = p.Status_ID;
-            product.PropertyType_ID = p.PropertyType_ID;
-            product.Create_post = p.Create_post;
-            product.Created_at = p.Created_at;
+            product.Price = p.Price;
+            product.UnitPrice = p.UnitPrice;
+            product.Area = p.Area;
+            product.BedRoom = p.BedRoom;
+            product.BathRoom = p.BathRoom;
+            product.PackingPlace = p.PackingPlace;
             product.Updated_at = DateTime.Now;
-           
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+            
+        }
+
+
+        private void AvatarU(PROPERTY p, out PROPERTY en, out string s)
+        {
+            en = db.PROPERTY.Find(p.ID);
+            string filename;
+            string extension;
+
+
             if (p.AvatarUpload != null)
             {
                 filename = Path.GetFileNameWithoutExtension(p.AvatarUpload.FileName);
@@ -72,50 +80,48 @@ namespace PPC.Areas.Admin.Controllers
                 s = p.Avatar;
                 filename = Path.Combine(Server.MapPath("~/Images"), filename);
                 p.AvatarUpload.SaveAs(filename);
-                product.Avatar = p.Avatar;
+
             }
             else
             {
-                product.Avatar =str;
+                s = en.Avatar;
+
+            }
+        }
+        private void ImagesU(PROPERTY p, out PROPERTY en, out string s)
+        {
+            en = db.PROPERTY.Find(p.ID);
+            string filename;
+            string extension;
+            string b;
+            s = "";
+
+            if (p.ImagesUpload == null)
+            {
+                s = en.Images;
 
             }
 
-
-            db.SaveChanges();
-
-            return RedirectToAction("Index");
-            
-        }
-
-
-        public string Images(PROPERTY p)
-        {
-
-            var product = db.PROPERTY.Find(p.ID);
-            string filename;
-            string extension;
-            string str = "";
-           // string avatar_old=p.Images;
-            if (p.ImagesUpload != null)
+            else
             {
+
                 foreach (var file in p.ImagesUpload)
                 {
+
                     filename = Path.GetFileNameWithoutExtension(file.FileName);
                     extension = Path.GetExtension(file.FileName);
                     filename = filename + DateTime.Now.ToString("yymmssff") + extension;
                     p.Images = filename;
-                    str = p.Images + ",";
+                    b = p.Images;
+                    s = string.Concat(s, b, ",");
                     filename = Path.Combine(Server.MapPath("~/Images"), filename);
                     file.SaveAs(filename);
                 }
 
-            }
-            else
-            {
-                str = product.Images;
+
+
             }
 
-            return str;
 
         }
 
